29a344620ea18c9c0ce28727bca36f462bac08b2
luchaking0day <kamilp20033@gmail.com>
2026-05-23 02:15:45 +0200

    Update banner.py

--- modules/banner.py
+++ modules/banner.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
 from modules.utils import Colors
 
-
-BANNER = r"""
- ██████╗ ███████╗ ██████╗ ██████╗ ███╗   ██╗██╗  ██╗
- ██╔══██╗██╔════╝██╔════╝██╔═══██╗████╗  ██║╚██╗██╔╝
- ██████╔╝█████╗  ██║     ██║   ██║██╔██╗ ██║ ╚███╔╝
- ██╔══██╗██╔══╝  ██║     ██║   ██║██║╚██╗██║ ██╔██╗
- ██║  ██║███████╗╚██████╗╚██████╔╝██║ ╚████║██╔╝ ██╗
- ╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═╝
+# Usunięto 'r' przed stringiem, aby kody ANSI (kolory) zadziałały
+BANNER = """
+██████╗ ███████╗ ██████╗ ██████╗ ███╗   ██╗\033[91m██╗  ██╗\033[96m
+██╔══██╗██╔════╝██╔════╝██╔═══██╗████╗  ██║\033[91m╚██╗██╔╝\033[96m
+██████╔╝█████╗  ██║     ██║   ██║██╔██╗ ██║\033[91m ╚███╔╝ \033[96m
+██╔══██╗██╔══╝  ██║     ██║   ██║██║╚██╗██║\033[91m ██╔██╗ \033[96m
+██║  ██║███████╗╚██████╗╚██████╔╝██║ ╚████║\033[91m██╔╝ ██╗\033[96m
+╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝\033[91m╚═╝  ╚═╝\033[0m
 """
 
-SUBTITLE = "   Domain & IP OSINT Reconnaissance Tool  v1.0"
-META     = "   by YourName | github.com/yourusername/reconx"
-
+SUBTITLE = "  Domain & IP OSINT Reconnaissance Tool  v1.0"
+META     = "  by luchaking0day | https://github.com/luchaking0day/reconx"
 
 def print_banner():
     print(f"{Colors.CYAN}{Colors.BOLD}{BANNER}{Colors.RESET}", end="")
